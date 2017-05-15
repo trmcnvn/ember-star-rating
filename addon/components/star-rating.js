d2545e3e80d12e92004bc286bdaf9823c6185363
sgault <sarah@bidvine.com>
2017-05-15 16:20:38 -0400

    Code review fixes

--- addon/components/star-rating.js
+++ addon/components/star-rating.js
@@ -1,8 +1,10 @@
 import Component from 'ember-component';
+import computed from 'ember-computed';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import { scheduleOnce } from 'ember-runloop';
 import { invokeAction } from 'ember-invoke-action';
+import { htmlSafe } from 'ember-string';
 import layout from '../templates/components/star-rating';
 
 const RatingComponent = Component.extend({
@@ -20,15 +22,15 @@ const RatingComponent = Component.extend({
   svgPath: 'M25.326,10.137c-0.117-0.361-0.431-0.625-0.807-0.68l-7.34-1.066l-3.283-6.651 c-0.337-0.683-1.456-0.683-1.793,0L8.82,8.391L1.48,9.457c-0.376,0.055-0.689,0.318-0.807,0.68c-0.117,0.363-0.02,0.76,0.253,1.025 l5.312,5.178l-1.254,7.31c-0.064,0.375,0.09,0.755,0.397,0.978c0.309,0.225,0.717,0.254,1.054,0.076L13,21.252l6.564,3.451 c0.146,0.077,0.307,0.115,0.466,0.115c0.207,0,0.413-0.064,0.588-0.191c0.308-0.223,0.462-0.603,0.397-0.978l-1.254-7.31 l5.312-5.178C25.346,10.896,25.443,10.5,25.326,10.137z',
   viewBox: '0 0 26 26',
 
-  style: Ember.computed('width', 'height', function() {
+  style: computed('width', 'height', function() {
     var style = '';
-    if (this.get('width')) {
-      style += `width: ${this.get('width')}; `;
+    if (get(this, 'width')) {
+      style += `width: ${get(this, 'width')}; `;
     }
-    if (this.get('height')) {
-      style += `height: ${this.get('height')};`;
+    if (get(this, 'height')) {
+      style += `height: ${get(this, 'height')};`;
     }
-    return style;
+    return htmlSafe(style);
   }),
 
   init() {

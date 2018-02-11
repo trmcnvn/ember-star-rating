fc6d62c8d59faced4a715da4f74fd4a83fd0ef55
Thomas McNiven <hello@vevix.net>
2018-02-11 15:09:25 +1000

    update to latest ember-cli

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,20 +1,18 @@
-/*jshint node:true*/
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      bower: {
-        dependencies: { }
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
+        }
       }
     },
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0'
         }
       }
     },
